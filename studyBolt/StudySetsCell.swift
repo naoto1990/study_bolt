f861d777dbc1b4ad3e25e3d1582b96507887de87
Naoto Ohno <ohno.naoto@jp.fujitsu.com>
2017-08-17 19:43:01 +0900

    feature_#1 StudySetsCell内のラベルを扱うファイルを作成

---
+++ studyBolt/StudySetsCell.swift
@@ -0,0 +1,24 @@
+//
+//  StudySetsCell.swift
+//  studyBolt
+//
+//  Created by Naoto Ohno on 2017/08/17.
+//  Copyright © 2017年 Naoto Ohno. All rights reserved.
+//
+
+import UIKit
+
+class StudySetsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
