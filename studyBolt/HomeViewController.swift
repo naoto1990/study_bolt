3619ef2383506931c604d9010319ccb7ec422251
Naoto Ohno <ohno.naoto@jp.fujitsu.com>
2017-08-18 14:45:47 +0900

    feature_#1 delegateメソッドを使用する宣言を設定していなかったので設定

--- studyBolt/HomeViewController.swift
+++ studyBolt/HomeViewController.swift
@@ -19,6 +19,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
