172b142fcd6fb9dcfa86801213da3bcc45d696a0
Naoto Ohno <ohno.naoto@jp.fujitsu.com>
2017-08-24 19:58:52 +0900

    feature_#2 scrollViewでのスワイプUIの実装途中コミット 問題: - storyboardのterm, definitionからAssCardViewクラスにIBOutletを接続できない

---
+++ studyBolt/AddCardView.swift
@@ -0,0 +1,15 @@
+//
+//  AddCardView.swift
+//  studyBolt
+//
+//  Created by Naoto Ohno on 2017/08/24.
+//  Copyright © 2017年 Naoto Ohno. All rights reserved.
+//
+
+import UIKit
+
+class AddCardView: UIView {
+    
+    
+
+}
--- studyBolt/CreateSetViewController.swift
+++ studyBolt/CreateSetViewController.swift
@@ -9,23 +9,46 @@
 import UIKit
 import RealmSwift
 
-class CreateSetViewController: UIViewController, UITextFieldDelegate {
+class CreateSetViewController: UIViewController, UITextFieldDelegate, UIScrollViewDelegate {
 
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var addCardView0: UIView!
-    @IBOutlet weak var addCardView1: UIView!
-    @IBOutlet weak var addCardView2: UIView!
+    @IBOutlet weak var addCardView0: AddCardView!
+    @IBOutlet weak var addCardView1: AddCardView!
+    @IBOutlet weak var addCardView2: AddCardView!
+
     
+    var cards = [Card()]
+    var cardIndex = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        scrollView.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        updateLocking()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func textFieldDidChange(_ notification: Notification) {
+        let currentCard = cards[cardIndex]
+//        currentCard.term = addCardView1.term.text
+//        currentCard.definition = addCardView1.definitionTextField.text
         
     }
     
@@ -55,4 +78,48 @@ class CreateSetViewController: UIViewController, UITextFieldDelegate {
     
     
 
+}
+
+
+extension CreateSetViewController {
+    
+//    func updateLocking() {
+//        if addCardView1.termTextField.text!.isEmpty && addCardView1.definitionTextField.text!.isEmpty {
+//            if cardIndex <= 0 {
+//                lockScrollView()
+//            } else {
+//                lockScrollViewRight()
+//            }
+//        } else {
+//            if cardIndex <= 0 {
+//                lockScrollViewLeft()
+//            } else {
+//                unlockScrollView()
+//            }
+//        }
+//    }
+//    
+//    func lockScrollView() {
+//        let insets = UIEdgeInsets(top: 0, left: -scrollView.frame.size.width, bottom: 0, right: -scrollView.frame.size.width)
+//        scrollView.contentInset = insets
+//    }
+//    
+//    func lockScrollViewRight() {
+//        let insets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: -scrollView.frame.size.width)
+//        scrollView.contentInset = insets
+//    }
+//    
+//    func lockScrollViewLeft() {
+//        let insets = UIEdgeInsets(top: 0, left: -scrollView.frame.size.width, bottom: 0, right: 0)
+//        scrollView.contentInset = insets
+//    }
+//    
+//    func unlockScrollView() {
+//        scrollView.contentInset = UIEdgeInsets.zero
+//    }
+//    
+//    func moveToCenter() {
+//        scrollView.contentOffset = CGPoint(x: scrollView.frame.size.width, y: 0)
+//    }
+    
 }
