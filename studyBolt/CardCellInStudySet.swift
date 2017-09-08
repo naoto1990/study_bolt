e1c346fb595893314c57d63f9478c4aaf2387378
Naoto Ohno <ohno.naoto@jp.fujitsu.com>
2017-09-08 16:45:15 +0900

    feature_#4 studySet画面のtbaleViewにカードのterm, definitionデータを表示

---
+++ studyBolt/CardCellInStudySet.swift
@@ -0,0 +1,16 @@
+//
+//  CardCellInStudySet.swift
+//  studyBolt
+//
+//  Created by Naoto Ohno on 2017/09/08.
+//  Copyright © 2017年 Naoto Ohno. All rights reserved.
+//
+
+import UIKit
+
+class CardCellInStudySet: UITableViewCell {
+
+    @IBOutlet weak var termInStudySet: UILabel!
+    @IBOutlet weak var definitionInStudySet: UILabel!
+
+}
--- studyBolt/StudySetViewController.swift
+++ studyBolt/StudySetViewController.swift
@@ -7,17 +7,23 @@
 //
 
 import UIKit
+import RealmSwift
 
 class StudySetViewController: UIViewController {
     
-    var selectedStudySet: StudySet?
-    
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var totalCardLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var selectedStudySet: StudySet?
+    let realm = try! Realm()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         
     }
     
@@ -32,7 +38,42 @@ class StudySetViewController: UIViewController {
     
     func displayStudySetInfo() {
         titleLabel.text = selectedStudySet?.title
+        let cards = realm.objects(Card.self).filter("studySetID = %@", selectedStudySet?.studySetID)
+        
+        totalCardLabel.text = String(cards.count)
     }
     
 
+}
+
+
+extension StudySetViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let cards = realm.objects(Card.self).filter("studySetID = %@", selectedStudySet?.studySetID)
+        
+        return cards.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cardsCell") as! CardCellInStudySet
+        
+        let cards = realm.objects(Card.self).filter("studySetID = %@", selectedStudySet?.studySetID)
+        print("kkkkkkk")
+        
+        cell.termInStudySet.text = cards[indexPath.row].term
+        cell.definitionInStudySet.text = cards[indexPath.row].definition
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+}
+
+
+extension StudySetViewController: UITableViewDelegate {
+    
 }
