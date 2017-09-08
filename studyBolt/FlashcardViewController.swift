394e46df1ce46065300695e78749e2471ff4e23e
Naoto Ohno <ohno.naoto@jp.fujitsu.com>
2017-09-08 18:16:27 +0900

    feature_#4 studySet画面からの画面遷移設定と遷移先のviewControllerファイルを作成

---
+++ studyBolt/FlashcardViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FlashcardViewController.swift
+//  studyBolt
+//
+//  Created by Naoto Ohno on 2017/09/08.
+//  Copyright © 2017年 Naoto Ohno. All rights reserved.
+//
+
+import UIKit
+
+class FlashcardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+}
