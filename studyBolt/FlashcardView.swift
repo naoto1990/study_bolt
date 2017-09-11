bdeede34745f7da77e749e899242299b6e25501a
Naoto Ohno <ohno.naoto@jp.fujitsu.com>
2017-09-11 17:39:01 +0900

    feature_#6 Flashcardの各viewを管理するFlashcardViewファイルを作成

---
+++ studyBolt/FlashcardView.swift
@@ -0,0 +1,16 @@
+//
+//  FlashcardView.swift
+//  studyBolt
+//
+//  Created by Naoto Ohno on 2017/09/11.
+//  Copyright © 2017年 Naoto Ohno. All rights reserved.
+//
+
+import UIKit
+
+class FlashcardView: UIView {
+
+    @IBOutlet var termInFlashCard: UILabel!
+    @IBOutlet var definitionInFlashCard: UILabel!
+
+}
