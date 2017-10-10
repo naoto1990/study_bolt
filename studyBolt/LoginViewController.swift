4d8839394fff10a3a0118711affac2b7e5e59cc7
Naoto Ohno <ohno.naoto@jp.fujitsu.com>
2017-10-10 12:17:01 +0900

    feature_#14 不要なコードを削除

--- studyBolt/LoginViewController.swift
+++ studyBolt/LoginViewController.swift
@@ -14,30 +14,12 @@ class LoginViewController: UIViewController {
     
     @IBAction func isLoggedInWithFB(_ sender: Any) {
         let loggedIn = AccessToken.current != nil
-        print(loggedIn)
-        
         if loggedIn {
             print("Logged in")
-            self.performSegue(withIdentifier: "toTabBarController", sender: nil)
         } else {
             print("Not logged in")
         }
         
-    }
-    
-    @IBAction func login(_ sender: UIButton) {
-        LoginManager().logIn([.email], viewController: self, completion: {
-            result in
-            switch result {
-            case let .success( permission, declinePemisson, token):
-                print("token:\(token),\(permission),\(declinePemisson)")
-            case let .failed(error):
-                print("error:\(error)")
-            case .cancelled:
-                print("cancelled")
-            }
-            
-        })
     }
     
     
@@ -46,8 +28,6 @@ class LoginViewController: UIViewController {
 
         let loginButton = LoginButton(readPermissions: [ .publicProfile ])
         loginButton.center = view.center
-//        loginButton.addTarget(self, action: "login", forControlEvents: UIControlEvents.TouchUpInside)
-        
         view.addSubview(loginButton)
     }
 
@@ -57,6 +37,5 @@ class LoginViewController: UIViewController {
     }
     
     }
-    */
 
 }
