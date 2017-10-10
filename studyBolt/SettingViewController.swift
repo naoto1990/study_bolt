70732bc3b732f53860ab9d9c348fe849ba50a697
Naoto Ohno <ohno.naoto@jp.fujitsu.com>
2017-10-10 14:43:30 +0900

    feature_#14 Setting画面内のLogoutボタンをFBSDKのデフォルトのものし、Logout時にloginButtonDidLogOutのdelegateメソッドが呼ばれるよう修正

--- studyBolt/SettingViewController.swift
+++ studyBolt/SettingViewController.swift
@@ -7,22 +7,21 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
+import FBSDKCoreKit
 import FBSDKLoginKit
 
+
 class SettingViewController: UIViewController {
-    @IBAction func logout(_ sender: Any) {
-        let loginManager : FBSDKLoginManager = FBSDKLoginManager()
-        loginManager.logOut()
-        
-        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let nextView = storyboard.instantiateInitialViewController()
-        present(nextView!, animated: true, completion: nil)
-    }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.delegate = self
+        loginButton.center = view.center
+        view.addSubview(loginButton)
 
     }
 
@@ -32,4 +31,19 @@ class SettingViewController: UIViewController {
     }
     
 
+}
+
+
+extension SettingViewController: LoginButtonDelegate {
+    func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
+        print("######Logged in######")
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: LoginButton) {
+        print("######Logged out######")
+        
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextView = storyboard.instantiateInitialViewController()
+        present(nextView!, animated: true, completion: nil)
+    }
 }
