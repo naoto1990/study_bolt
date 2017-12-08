1f0b07cf15ebd933f46f413dc41946ff177673ad
Naoto Ohno <ohno.naoto@jp.fujitsu.com>
2017-12-08 18:43:23 +0900

    feature_#17 setting画面にgroupedスタイルのtableViewを追加

--- studyBolt/SettingViewController.swift
+++ studyBolt/SettingViewController.swift
@@ -13,10 +13,20 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 
 
+var TableTitle = [ ["menuTitle01", "title01", "title02"], ["menuTitle02", "title03", "title04"], ["menuTitle03", "menuTitle05", "menuTitle06"], ["menuTitle04", "menuTitle07"] ]
+
+var TableSubtitle = [ ["", "subtitle02", "subtitle03"], ["","subtitle05", "subtitle06"], ["", "subtitle06", "subtitle07"], ["", "subtitle08"] ]
+
+
 class SettingViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         
         let loginButton = LoginButton(readPermissions: [ .publicProfile ])
         loginButton.delegate = self
@@ -45,5 +55,41 @@ extension SettingViewController: LoginButtonDelegate {
         let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let nextView = storyboard.instantiateInitialViewController()
         present(nextView!, animated: true, completion: nil)
+    }
+}
+
+
+extension SettingViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // studySetCollectionの総数を返却
+        return TableTitle[section].count - 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: "settingCell")
+        cell.textLabel?.text = TableTitle[indexPath.section][indexPath.row + 1]
+        cell.textLabel?.text = TableTitle[indexPath.section][indexPath.row + 1]
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return TableTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return TableTitle[section][0]
+        
+    }
+    
+    
+}
+
+
+extension SettingViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
